a4bd3b9a055f860e8c62138739464a98ba6c4e02
GiovanniCardinali <giovanni.cardinali@studenti.unicam.it>
2026-05-26 17:19:44 +0200

    created TimerService.java

---
+++ src/main/java/org/example/progettotasktree/Service/TimerService.java
@@ -0,0 +1,43 @@
+package org.example.progettotasktree.Service;
+
+import org.example.progettotasktree.Entity.Timer;
+import org.example.progettotasktree.Repo.TimerRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Service
+public class TimerService {
+    private TimerRepository timerRepository;
+
+    public UUID avvioTimer(int dipendente){
+
+        Timer timer = timerRepository.findByDipendenteGiorno(dipendente, LocalDate.now()).orElse(null);
+        if(timer == null){
+            throw new RuntimeException();
+        }
+
+        Timer nuovoTimer = new Timer();
+        nuovoTimer.setDipendente(dipendente);
+        timerRepository.save(nuovoTimer);
+        return nuovoTimer.getId();
+    }
+
+    public void fermaTimer(int dipendente){
+        //recupero il timer dalla repo e lo salvo in appoggio
+        Timer timer = timerRepository.findByDipendenteGiorno(dipendente, LocalDate.now()).orElse(null);
+
+        if(timer == null){
+            throw new RuntimeException();
+        }
+        else if(timer.getOraFine() != null){
+            throw new RuntimeException();
+        }
+
+        //setto la ora di fine
+        timer.setOraFine();
+        timerRepository.save(timer);
+    }
+
+}
