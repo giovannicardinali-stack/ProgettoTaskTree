4d9b10e57d0724dae511d550c498ba719e5591de
GiovanniCardinali <giovanni.cardinali@studenti.unicam.it>
2026-05-26 16:08:00 +0200

    Initial commit

---
+++ src/main/java/org/example/progettotasktree/Entity/Timer.java
@@ -0,0 +1,53 @@
+package Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.UUID;
+
+
+@Entity
+public class Timer {
+
+    @Id
+    private UUID id;
+    private int dipendente; //aggiungi poi dipendente
+    private LocalDate Giorno;
+    private LocalTime oraInizio;
+    private LocalTime oraFine;
+
+    public Timer() {
+        this.id = UUID.randomUUID();
+        this.dipendente = 0;
+        this.Giorno = LocalDate.now();
+        this.oraInizio = LocalTime.now();
+    }
+
+    public void setOraFine(){
+        this.oraFine = LocalTime.now();
+    }
+
+    public void setDipendente(int dipendente){
+        this.dipendente = dipendente;
+    }
+
+
+    public UUID getId(){
+        return id;
+    }
+
+    public int getDipendente(){
+        return dipendente;
+    }
+    public LocalDate getGiorno(){
+        return Giorno;
+    }
+    public LocalTime getOraInizio(){
+        return oraInizio;
+    }
+    public LocalTime getOraFine(){
+        return oraFine;
+    }
+}
---
+++ src/main/java/org/example/progettotasktree/ProgettoTaskTreeApplication.java
@@ -0,0 +1,13 @@
+package org.example.progettotasktree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoTaskTreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoTaskTreeApplication.class, args);
+	}
+
+}
